8d5cc44d86313cc61db1a896d877885b4c7170ff
Nick Korolev <nick.korolev@fundraiseup.com>
2023-06-17 16:54:47 +0200

    add deploy example

--- examples/vue-example/src/App.vue
+++ examples/vue-example/src/App.vue
@@ -16,6 +16,9 @@ const onConfirmClick = async () => {
     cancelText: 'Nope',
     closable: true,
   });
+  setTimeout(() => {
+    isConfirmed.value = false;
+  }, 3000);
 }
 
 const onAlertClick = async () => {
@@ -52,20 +55,61 @@ const onRootAlertClick = async () => {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <div>
-        <button @click="onConfirmClick">Confirm</button>
-        <p v-if="isConfirmed">Confirmed</p>
+      <div class="confirm">
+        <button class="button" @click="onConfirmClick">{{ isConfirmed ? 'Confirmed' : 'Confirm' }}</button>
       </div>
-      <div>
-        <button @click="onAlertClick">Alert</button>
+      <div class="alert">
+        <button class="button" @click="onAlertClick">Alert</button>
+        <div ref="rootContainer" style="margin-left: 12px">
+          <button class="button" @click="onRootAlertClick">Root Alert</button>
+        </div>
       </div>
-      <div>
-        <button @click="onPromptClick">Prompt</button>
-        <p v-if="promptValue">{{ promptValue }}</p>
-      </div>
-      <div ref="rootContainer">
-        <button @click="onRootAlertClick">Root Alert</button>
+      <div class="prompt">
+        <button class="button" @click="onPromptClick">Prompt</button>
+        <div style="margin-left: 12px" v-if="promptValue">{{ promptValue }}</div>
       </div>
     </div>
   </header>
 </template>
+
+<style>
+.button {
+  background-color: #24292e;
+  border: 1px solid rgba(27,31,35,.2);
+  border-radius: .25em;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  padding: 6px 12px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+}
+button:hover {
+  background-color: #2f363d;
+  border-color: rgba(27,31,35,.5);
+  color: #fff;
+  text-decoration: none;
+}
+.confirm {
+  margin-top: 12px;
+  display: flex;
+  align-items: center;
+}
+.alert {
+  margin-top: 12px;
+  display: flex;
+  align-items: center;
+}
+.prompt {
+  margin-top: 12px;
+  display: flex;
+  align-items: center;
+}
+</style>
